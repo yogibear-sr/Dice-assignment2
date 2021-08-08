6eaee087db7b485e2a64f4935e4aecba88a03e4b
yogibear-sr <60042822+yogibear-sr@users.noreply.github.com>
2021-08-08 20:59:36 +0100

    added check for more than 1 output file

--- dice_summary.py
+++ dice_summary.py
@@ -17,30 +17,41 @@ def lambda_handler(event, context):
     resp = s3.list_objects_v2(Bucket=bucket)
     for obj in resp['Contents']:
         keys.append(obj['Key'])
-
-    stream = s3.get_object(Bucket=bucket, Key=keys[1])
-    contents = stream['Body'].read()
-    data = json.loads(contents)
-    Rolls=returnSum(data)
-
-    d1=data
-    keys.pop(1)
-
-    for i in range(len(keys)):
-        stream = s3.get_object(Bucket=bucket, Key=keys[i])
+        
+    if len(keys) > 1:
+        
+        stream = s3.get_object(Bucket=bucket, Key=keys[1])
         contents = stream['Body'].read()
         data = json.loads(contents)
-        Rolls = Rolls + returnSum(data)
+        Rolls=returnSum(data)
+        
+        d1=data
+    
+        keys.pop(1)
+    
+        print(keys)
+    
+        exit()
+
+        for i in range(len(keys)):
+            stream = s3.get_object(Bucket=bucket, Key=keys[i])
+            contents = stream['Body'].read()
+            data = json.loads(contents)
+            Rolls = Rolls + returnSum(data)
 
 # now merge the two arrays using Counter
-        d1 = Counter(d1)+Counter(data)
+            d1 = Counter(d1)+Counter(data)
 
-    print("TOTAL number of rolls is ", Rolls)
+            print("TOTAL number of rolls is ", Rolls)
 
-    # go through each element in the array summarise and print
-    for key,value in d1.items():
-        percent = (value / Rolls)*100
-        print("The number ",key, "was rolled ",value, "times, which is ", round(percent,2),"%")
+# go through each element in the array summarise and print
+            for key,value in d1.items():
+                percent = (value / Rolls)*100
+                print("The number ",key, "was rolled ",value, "times, which is ", round(percent,2),"%")
+                
+    else:
+        print("Not enough files to compare")
+        print("Roll the dice a few more times")
 
     return {
         'statusCode': 200,
